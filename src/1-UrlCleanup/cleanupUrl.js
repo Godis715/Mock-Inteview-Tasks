f2da83794addea0ee53a19551257ad3eb49ede54
hitgroover <fgro4chan@gmail.com>
2023-02-03 22:08:25 +0300

    first exercise

--- src/1-UrlCleanup/cleanupUrl.js
+++ src/1-UrlCleanup/cleanupUrl.js
@@ -1,5 +1,37 @@
 export default function cleaupUrl(url) {
-  // TODO: реализовать функцию, удаляющую повторяющиеся
-  // слэши внутри URL (двойной слэш после схемы нужно сохранить)
-  return url;
+   let previousSymb;
+   let result='';
+   if (url.startsWith('http://')){
+      for (let i = 7; i < url.length; i++){
+        if (url[i] === '/' && previousSymb === '/'){
+         continue;
+        }
+        result += url[i];
+        previousSymb = url[i];
+      }
+   } else if (url.startsWith('https://')){
+      for (let i = 8; i < url.length; i++){
+         if (url[i] === '/' && previousSymb === '/'){
+            continue;
+           }
+           result += url[i];
+           previousSymb = url[i];
+      }
+   } else {
+      if (url[i] === '/' && previousSymb === '/'){
+         result += url[i];
+        previousSymb = url[i];
+        }
+        
+   }
+   if (url.startsWith('http://')){
+      return ('http://' + result);
+   } else if (url.startsWith('https://')) {
+      return ('https://' + result);
+   } else
+  return result;
 }
+
+let url = "https://some-domain.com//account//settings";
+let result = cleaupUrl(url);
+console.log (result);
