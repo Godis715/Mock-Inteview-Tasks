7d3ac299087f74595d268321b380eeed6722d8a6
Denis Ignatev <godis7.15@gmail.com>
2023-02-03 23:09:18 +0300

    Revert "first exercise"
    This reverts commit dc52976630a1dbd115c27dd48a6f3f0b7da63a2c.

--- src/1-UrlCleanup/cleanupUrl.js
+++ src/1-UrlCleanup/cleanupUrl.js
@@ -1,37 +1,5 @@
 export default function cleaupUrl(url) {
-   let previousSymb;
-   let result='';
-   if (url.startsWith('http://')){
-      for (let i = 7; i < url.length; i++){
-        if (url[i] === '/' && previousSymb === '/'){
-         continue;
-        }
-        result += url[i];
-        previousSymb = url[i];
-      }
-   } else if (url.startsWith('https://')){
-      for (let i = 8; i < url.length; i++){
-         if (url[i] === '/' && previousSymb === '/'){
-            continue;
-           }
-           result += url[i];
-           previousSymb = url[i];
-      }
-   } else {
-      if (url[i] === '/' && previousSymb === '/'){
-         result += url[i];
-        previousSymb = url[i];
-        }
-        
-   }
-   if (url.startsWith('http://')){
-      return ('http://' + result);
-   } else if (url.startsWith('https://')) {
-      return ('https://' + result);
-   } else
-  return result;
+  // TODO: реализовать функцию, удаляющую повторяющиеся
+  // слэши внутри URL (двойной слэш после схемы нужно сохранить)
+  return url;
 }
-
-let url = "https://some-domain.com//account//settings";
-let result = cleaupUrl(url);
-console.log (result);
